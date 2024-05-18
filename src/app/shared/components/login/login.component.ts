9d84f9020cc1ca841e12b666f7774ebaf8583f59
Alvarocg15 <alvarocedazo@gmail.com>
2024-05-18 23:39:52 +0200

    feat: Add jwt-decode library for decoding JWT tokens

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,9 +1,81 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from '../../../../enviroments/enviroment';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CustomJwtPayload } from '../../../interface/CustomJwtPayload.interface';
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  rememberMe = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    public dialogRef: MatDialogRef<LoginComponent>,
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      rememberMe: [false],
+    });
 
+    const rememberMeControl = this.loginForm.get('rememberMe');
+    if (rememberMeControl) {
+      rememberMeControl.valueChanges.subscribe((remember) => {
+        this.rememberMe = remember;
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+
+      this.http.post<any>(`${environment.apiUrl}/login`, formData).subscribe(
+        (response) => {
+          if (response && response.token) {
+            this.dialogRef.close();
+            // Decodificar el token
+            const decodedToken = jwtDecode<CustomJwtPayload>(response.token);
+
+            // Verificar el rol del usuario
+            if (decodedToken.authorities.includes('ROLE_ADMIN')) {
+              // Si el usuario es un administrador, redirigir a /administracion
+              this.router.navigate(['/administracion']);
+            } else {
+              // Si el usuario no es un administrador, redirigir a /cuenta
+              this.router.navigate(['/cuenta']);
+            }
+
+            if (this.rememberMe) {
+              localStorage.setItem('token', response.token);
+            } else {
+              sessionStorage.setItem('token', response.token);
+            }
+
+            this.snackBar.open('Inicio de sesión exitoso', 'Cerrar', {
+              duration: 3000,
+              horizontalPosition: 'right',
+            });
+          }
+        },
+        (error) => {
+          if (error.status === 401) {
+            this.snackBar.open('Usuario o contraseña incorrectos', 'Cerrar', {
+              duration: 3000,
+              horizontalPosition: 'right',
+            });
+          }
+        }
+      );
+    }
+  }
 }
